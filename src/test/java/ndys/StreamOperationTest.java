9e525ec262161ee51172b0a3001d9ebe929e332a
ndys <hendy.sg02@gmail.com>
2025-03-17 20:07:52 +0700

    [feat] stream operation

---
+++ src/test/java/ndys/StreamOperationTest.java
@@ -0,0 +1,21 @@
+package ndys;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamOperationTest {
+
+    @Test
+    void testMapStream() {
+        List<String> names = List.of("Hendy", "Saputra");
+
+        // Names is immutable, stream create new transformed data
+        Stream<String> streamNames = names.stream();
+        Stream<String> streamUpper = streamNames.map(String::toUpperCase);
+
+        streamUpper.forEach(System.out::println);
+        names.forEach(System.out::println);
+    }
+}
